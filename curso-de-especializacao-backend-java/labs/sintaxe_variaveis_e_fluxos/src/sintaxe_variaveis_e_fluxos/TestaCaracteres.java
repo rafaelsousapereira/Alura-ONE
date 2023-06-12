f5b3c227a825e25725c76427f4f0b5c08ffdf498
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2023-06-12 19:06:46 -0300

    feat: Caracteres

---
+++ curso-de-especializacao-backend-java/labs/sintaxe_variaveis_e_fluxos/src/sintaxe_variaveis_e_fluxos/TestaCaracteres.java
@@ -0,0 +1,22 @@
+package sintaxe_variaveis_e_fluxos;
+
+public class TestaCaracteres {
+	
+	public static void main(String[] args) {
+		char letra = 'a';
+		System.out.println(letra);
+		
+		char valor = 66;
+		System.out.println(valor);
+		
+		valor = (char) (valor + 1);
+		System.out.println(valor);
+		
+		String palavra = "Alura cusos online de tecnologia";
+		System.out.println(palavra);
+		
+		palavra = palavra + 2020;
+		System.out.println(palavra);
+	}
+
+}
---
+++ curso-de-especializacao-backend-java/labs/sintaxe_variaveis_e_fluxos/src/sintaxe_variaveis_e_fluxos/TestaValores.java
@@ -0,0 +1,14 @@
+package sintaxe_variaveis_e_fluxos;
+
+public class TestaValores {
+
+	public static void main(String[] args) {
+		int primeiro = 5;
+		int segundo = 7;
+		segundo = primeiro;
+		
+		primeiro = 10;
+		
+		System.out.println(segundo);
+	}
+}

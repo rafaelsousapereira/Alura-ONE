ef39b36d79f1a082379f171858f19c6c9cf4e9e2
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2023-06-12 18:47:12 -0300

    feat: Tipos e variaveis

---
+++ curso-de-especializacao-backend-java/labs/sintaxe_variaveis_e_fluxos/src/sintaxe_variaveis_e_fluxos/TestaPontoFlutuante.java
@@ -0,0 +1,19 @@
+package sintaxe_variaveis_e_fluxos;
+
+public class TestaPontoFlutuante {
+	
+	public static void main(String[] args) {
+		
+		double salario = 1250.70;
+		System.out.printf("Meu salario é: %1.2f%n", salario);
+		
+		double divisao = 3.14 / 2;
+		System.out.println(divisao);
+		
+		int outraDivisao = 5 / 2;
+		System.out.println(outraDivisao);
+		
+		double novaTentativa = 5.0 / 2;
+		System.out.println(novaTentativa);
+	}
+}

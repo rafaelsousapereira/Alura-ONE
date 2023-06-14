e9a2420815b7edce781d7f3c929a33549771e3c4
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2023-06-14 20:37:02 -0300

    feat: Getters e Setters de referência

--- curso-de-especializacao-backend-java/labs/bytebanco-encapsulamento/src/Conta.java
+++ curso-de-especializacao-backend-java/labs/bytebanco-encapsulamento/src/Conta.java
@@ -46,4 +46,12 @@ public class Conta {
 	public void setAgencia(int agencia) {
 		this.agencia = agencia;
 	}
+	
+	public Cliente getTitular() {
+		return this.titular;
+	}
+	
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
 }
--- curso-de-especializacao-backend-java/labs/bytebanco-encapsulamento/src/TestaGettersESetters.java
+++ curso-de-especializacao-backend-java/labs/bytebanco-encapsulamento/src/TestaGettersESetters.java
@@ -10,6 +10,24 @@ public class TestaGettersESetters {
 		
 		System.out.println("Número da Conta: " + conta.getNumero());
 		System.out.println("Número da Agência: " + conta.getAgencia());
+		
+		Cliente maria = new Cliente();
+		maria.setNome("Maria Aparecida");
+		
+		conta.setTitular(maria);
+		System.out.println("Nome Cliente: " + conta.getTitular().getNome());
+		
+		conta.getTitular().setProfissao("Programadora");
+		System.out.println("Profissão Cliente: " + conta.getTitular().getProfissao());
+		
+		Cliente titularDaConta = conta.getTitular();
+		titularDaConta.setCpf("111.111.111-11");
+		
+		System.out.println("CPF Cliente: " + conta.getTitular().getCpf());
+		
+		System.out.println(conta.getTitular());
+		System.out.println(maria);
+		System.out.println(titularDaConta);
 	}
 
 }

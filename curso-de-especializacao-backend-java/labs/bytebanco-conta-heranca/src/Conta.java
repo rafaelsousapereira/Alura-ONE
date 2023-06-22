4c64490584ffad445fbc6ec777d503b565f918c1
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2023-06-22 15:15:55 -0300

    feat: heranca e contrutores

---
+++ curso-de-especializacao-backend-java/labs/bytebanco-conta-heranca/src/Conta.java
@@ -0,0 +1,78 @@
+
+public class Conta {
+
+	private double saldo;
+	private int agencia;
+	private int numero;
+	private Cliente titular;
+	private static int total = 0;
+	
+	public Conta(int agencia, int numero) {
+		Conta.total++;
+		System.out.println("o total de contas eh: " + Conta.total);
+		this.agencia = agencia;
+		this.numero = numero;
+		System.out.println("estou criando uma conta = " + this.numero);
+	}
+
+	void deposita(double valor) {
+		this.saldo += valor;
+	}
+
+	public boolean saca(double valor) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		}
+		return false;
+	}
+
+	public boolean transfere(double valor, Conta destino) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			destino.deposita(valor);
+			return true;
+		}
+		return false;
+	}
+	
+	public double getSaldo() {
+		return this.saldo;
+	}
+
+	public int getNumero() {
+		return this.numero;
+	}
+	
+	public void setNumero(int numero) {
+		if (numero <= 0) {
+			System.out.println("nao pode numero menor igual a 0");
+			return;
+		}
+		this.numero = numero;
+	}
+	
+	public int getAgencia() {
+		return this.agencia;
+	}
+	
+	public void setAgencia(int agencia) {
+		if (agencia <= 0) {
+			System.out.println("nao pode agencia menor igual a 0");
+			return;
+		}
+		this.agencia = agencia;
+	}
+	
+	public Cliente getTitular() {
+		return this.titular;
+	}
+	
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+	
+	public static int getTotal() {
+		return total;
+	}
+}

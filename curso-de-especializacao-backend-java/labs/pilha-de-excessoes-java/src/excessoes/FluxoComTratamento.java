e2d74093cafa00241b55441a5c65449e3f844169
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2023-07-17 20:19:01 -0300

    feat: lancando excecoes

---
+++ curso-de-especializacao-backend-java/labs/pilha-de-excessoes-java/src/excessoes/FluxoComTratamento.java
@@ -0,0 +1,30 @@
+package excessoes;
+
+public class FluxoComTratamento {
+
+	public static void main(String[] args) {
+        System.out.println("Ini do main");
+        try {
+        	metodo1();
+        } catch (ArithmeticException | NullPointerException ex) {
+        	String mensagem = ex.getMessage();
+			System.out.println("Exception " + mensagem);
+			ex.printStackTrace();
+		}
+        System.out.println("Fim do main");
+    }
+
+    private static void metodo1() {
+        System.out.println("Ini do metodo1");
+    	metodo2();
+        System.out.println("Fim do metodo1");
+    }
+
+    private static void metodo2() {
+        System.out.println("Ini do metodo2");
+        
+        throw new ArithmeticException("Deu ruim");
+        
+//        System.out.println("Fim do metodo2");
+    }
+}

e122103b2503e2016c7fe1b86cc666d63d73b6ab
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2023-07-18 18:53:34 -0300

    feat: try with resources

--- curso-de-especializacao-backend-java/labs/pilha-de-excessoes-java/src/excessoes/Conexao.java
+++ curso-de-especializacao-backend-java/labs/pilha-de-excessoes-java/src/excessoes/Conexao.java
@@ -1,6 +1,6 @@
 package excessoes;
 
-public class Conexao {
+public class Conexao implements AutoCloseable {
 
     public Conexao() {
         System.out.println("Abrindo conexao");
@@ -8,10 +8,11 @@ public class Conexao {
 
     public void leDados() {
         System.out.println("Recebendo dados");
-            throw new IllegalStateException();
+        throw new IllegalStateException();
     }
 
-    public void fecha() {
-        System.out.println("Fechando conexao");
-    }
+	@Override
+	public void close() {
+		System.out.println("Fechando conexao");
+	}
 }
--- curso-de-especializacao-backend-java/labs/pilha-de-excessoes-java/src/excessoes/TesteConexao.java
+++ curso-de-especializacao-backend-java/labs/pilha-de-excessoes-java/src/excessoes/TesteConexao.java
@@ -4,15 +4,23 @@ public class TesteConexao {
 
 	public static void main(String[] args) {
 		
-		Conexao con = null;
-				
-		try {
-			con = new Conexao();
-			con.leDados();
+		try (Conexao conexao = new Conexao()) {
+			conexao.leDados();
 		} catch (IllegalStateException ex) {
 			System.out.println("Deu erro na conexao ...");
-		} finally {
-			con.fecha();
 		}
+
+//		------------------------------------------
+		
+//		Conexao con = null;
+//				
+//		try {
+//			con = new Conexao();
+//			con.leDados();
+//		} catch (IllegalStateException ex) {
+//			System.out.println("Deu erro na conexao ...");
+//		} finally {
+//			con.close();
+//		}
 	}
 }

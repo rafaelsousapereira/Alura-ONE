c9d9559c0df73dfa848ad6adfb7376ab22a35efd
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2023-07-21 20:51:58 -0300

    feat: arrays

--- curso-de-especializacao-backend-java/labs/bytebanco-conta-heranca/src/br/com/bytebanco/banco/teste/Teste.java
+++ curso-de-especializacao-backend-java/labs/bytebanco-conta-heranca/src/br/com/bytebanco/banco/teste/Teste.java
@@ -1,21 +1,19 @@
 package br.com.bytebanco.banco.teste;
 
-import br.com.bytebanco.banco.modelo.ContaCorrente;
-import br.com.bytebanco.banco.modelo.ContaPoupanca;
-
 public class Teste {
-
-	public static void main(String[] args) {
-		
-		Object cc = new ContaCorrente(11, 12);
-		Object cp = new ContaPoupanca(22, 23);
-
-		System.out.println(cc);
-		System.out.println(cp);
-		
-//		print(cc);
-	}
 	
-	public static void print(Object ref) {}
+	// Array []
+	public static void main(String[] args) {
+
+		int[] idades = new int[5]; // inicializa o array com 0
+		
+		for (int i = 0; i < idades.length; i++) {
+			idades[i] = i * i;
+		}
+		
+		for (int i = 0; i < idades.length; i++) {
+			System.out.println(idades[i]);
+		}
+	}
 
 }
---
+++ curso-de-especializacao-backend-java/labs/bytebanco-conta-heranca/src/br/com/bytebanco/banco/teste/TesteObject.java
@@ -0,0 +1,21 @@
+package br.com.bytebanco.banco.teste;
+
+import br.com.bytebanco.banco.modelo.ContaCorrente;
+import br.com.bytebanco.banco.modelo.ContaPoupanca;
+
+public class TesteObject {
+
+	public static void main(String[] args) {
+		
+		Object cc = new ContaCorrente(11, 12);
+		Object cp = new ContaPoupanca(22, 23);
+
+		System.out.println(cc);
+		System.out.println(cp);
+		
+//		print(cc);
+	}
+	
+	public static void print(Object ref) {}
+
+}

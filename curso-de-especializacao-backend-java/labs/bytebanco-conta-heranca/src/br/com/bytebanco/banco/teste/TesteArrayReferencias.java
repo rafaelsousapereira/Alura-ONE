06a4c60b075743e68ee831e7747f5214a19d5da7
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2023-07-21 21:41:51 -0300

    feat: array tipo object

--- curso-de-especializacao-backend-java/labs/bytebanco-conta-heranca/src/br/com/bytebanco/banco/teste/TesteArrayReferencias.java
+++ curso-de-especializacao-backend-java/labs/bytebanco-conta-heranca/src/br/com/bytebanco/banco/teste/TesteArrayReferencias.java
@@ -1,23 +1,27 @@
 package br.com.bytebanco.banco.teste;
 
 import br.com.bytebanco.banco.modelo.ContaCorrente;
+import br.com.bytebanco.banco.modelo.ContaPoupanca;
 
 public class TesteArrayReferencias {
 
 	public static void main(String[] args) {
 		
-		ContaCorrente[] contas = new ContaCorrente[5];
+		Object[] referencias = new Object[5];
 		
 		ContaCorrente cc1 = new ContaCorrente(1, 1);
-		contas[0] = cc1;
+		referencias[0] = cc1;
 		
-		ContaCorrente cc2 = new ContaCorrente(2, 2);
-		contas[1] = cc2;
+		ContaPoupanca cp2 = new ContaPoupanca(2, 2);
+		referencias[1] = cp2;
 		
-		System.out.println( contas[0].getNumero() );
+//		Object referenciaGenerica = referencias[1];
+//		
+//		System.out.println( referencias.getNumero() );
 		
 		// Destrinchando a chamada: contas[0].getNumero();
-		ContaCorrente ref = contas[0];
+		ContaPoupanca ref = (ContaPoupanca) referencias[1]; // type cast
+		System.out.println(cp2.getNumero());
 		System.out.println(ref.getNumero());
 	}
 

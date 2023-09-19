7c525913cab6be8d24dc78292cd76bb5c348bb4c
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2023-09-19 16:51:54 -0300

    feat: searching entities

--- curso-de-especializacao-backend-java/labs/loja/src/main/java/br/com/alura/loja/dao/ProdutoDAO.java
+++ curso-de-especializacao-backend-java/labs/loja/src/main/java/br/com/alura/loja/dao/ProdutoDAO.java
@@ -1,5 +1,7 @@
 package br.com.alura.loja.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import br.com.alura.loja.modelo.Produto;
@@ -14,6 +16,15 @@ public class ProdutoDAO {
 
 	public void cadastrar(Produto produto) {
 		this.entityManager.persist(produto);
+	}
+	
+	public Produto buscarPorId(Long id) {
+		return entityManager.find(Produto.class, id);
+	}
+	
+	public List<Produto> buscarTodos() {
+		String jpql = "SELECT p FROM Produto p";
+		return entityManager.createQuery(jpql, Produto.class).getResultList();
 	}
 
 }
--- curso-de-especializacao-backend-java/labs/loja/src/main/java/br/com/alura/loja/testes/CadastroDeProdutos.java
+++ curso-de-especializacao-backend-java/labs/loja/src/main/java/br/com/alura/loja/testes/CadastroDeProdutos.java
@@ -1,30 +1,47 @@
 package br.com.alura.loja.testes;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
+import br.com.alura.loja.dao.CategoriaDAO;
+import br.com.alura.loja.dao.ProdutoDAO;
 import br.com.alura.loja.modelo.Categoria;
+import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
 
 public class CadastroDeProdutos {
 
 	public static void main(String[] args) {
-		Categoria celulares = new Categoria("CELULARES");
+		cadastrarProduto();
 
 		EntityManager entityManager = JPAUtil.getEntityManager();
+		ProdutoDAO produtoDAO = new ProdutoDAO(entityManager);
+		
+		Produto produto = produtoDAO.buscarPorId(1l);
+		System.out.println(produto.getPreco());
+		
+		List<Produto> todosOsProdutos = produtoDAO.buscarTodos();
+		todosOsProdutos.stream().forEach(p -> System.out.println(p.getNome()));
+	}
+
+	private static void cadastrarProduto() {
+		Categoria celulares = new Categoria("CELULARES");
+		Produto celular = new Produto("Xaomi Redmi", "Muito legal", new BigDecimal("800"), celulares);
+
+		EntityManager entityManager = JPAUtil.getEntityManager();
+
+		ProdutoDAO produtoDAO = new ProdutoDAO(entityManager);
+		CategoriaDAO categoriaDAO = new CategoriaDAO(entityManager);
+
 		entityManager.getTransaction().begin();
-		
-		entityManager.persist(celulares);
-		celulares.setNome("XPTO");
-		
-		entityManager.flush();
-		entityManager.clear();
-		
-		celulares = entityManager.merge(celulares);
-		celulares.setNome("1234");
-		entityManager.flush();
-		
-		entityManager.remove(celulares);
-		entityManager.flush();
+
+		categoriaDAO.cadastrar(celulares);
+		produtoDAO.cadastrar(celular);
+
+		entityManager.getTransaction().commit();
+		entityManager.close();
 	}
 
 }

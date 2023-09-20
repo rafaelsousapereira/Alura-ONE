0fc1b8488cddc2604e923daf16c52ef2713aec2e
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2023-09-20 13:39:06 -0300

    feat: relationship many-to-many

--- curso-de-especializacao-backend-java/labs/loja/src/main/java/br/com/alura/loja/modelo/Pedido.java
+++ curso-de-especializacao-backend-java/labs/loja/src/main/java/br/com/alura/loja/modelo/Pedido.java
@@ -2,12 +2,14 @@ package br.com.alura.loja.modelo;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -22,6 +24,9 @@ public class Pedido {
 
 	@ManyToOne
 	private Cliente cliente;
+	
+	@OneToMany
+	private List<ItemPedido> items;
 
 	public Pedido() {
 	}

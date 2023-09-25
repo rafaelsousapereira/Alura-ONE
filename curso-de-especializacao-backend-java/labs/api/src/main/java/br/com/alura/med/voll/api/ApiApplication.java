1aef1ef2bbef06add78a1cf4cd0e8f40f143df38
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2023-09-25 13:56:00 -0300

    feat: starting new project spring boot

---
+++ curso-de-especializacao-backend-java/labs/api/src/main/java/br/com/alura/med/voll/api/ApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.med.voll.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}

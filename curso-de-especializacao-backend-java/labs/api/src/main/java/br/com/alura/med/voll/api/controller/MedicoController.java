a0ec1ac3b5a6a78611de7ebe2edbba60b2d9c492
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2023-09-26 18:06:04 -0300

    feat: pagination and ordering

--- curso-de-especializacao-backend-java/labs/api/src/main/java/br/com/alura/med/voll/api/controller/MedicoController.java
+++ curso-de-especializacao-backend-java/labs/api/src/main/java/br/com/alura/med/voll/api/controller/MedicoController.java
@@ -6,10 +6,11 @@ import br.com.alura.med.voll.api.medico.Medico;
 import br.com.alura.med.voll.api.medico.MedicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -25,8 +26,8 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DadosListagemMedico> listar() {
-        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 
 }

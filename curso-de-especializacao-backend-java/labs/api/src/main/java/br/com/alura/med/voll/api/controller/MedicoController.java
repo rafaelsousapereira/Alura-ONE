2def704d5e8f7892ccf149eb8a581a746441daf4
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2023-09-26 15:43:05 -0300

    feat: validations with Bean Validation

--- curso-de-especializacao-backend-java/labs/api/src/main/java/br/com/alura/med/voll/api/controller/MedicoController.java
+++ curso-de-especializacao-backend-java/labs/api/src/main/java/br/com/alura/med/voll/api/controller/MedicoController.java
@@ -3,6 +3,7 @@ package br.com.alura.med.voll.api.controller;
 import br.com.alura.med.voll.api.medico.DadosCadastroMedico;
 import br.com.alura.med.voll.api.medico.Medico;
 import br.com.alura.med.voll.api.medico.MedicoRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,7 +20,7 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody DadosCadastroMedico dados) {
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
     }
 

8f910bb836088cf076655edaa885c5807cf1cbc4
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2023-09-26 18:21:08 -0300

    feat: implemente list, pagination and ordering from pacientes

---
+++ curso-de-especializacao-backend-java/labs/api/src/main/java/br/com/alura/med/voll/api/paciente/DadosListagemPaciente.java
@@ -0,0 +1,8 @@
+package br.com.alura.med.voll.api.paciente;
+
+public record DadosListagemPaciente(String nome, String email, String cpf) {
+
+    public DadosListagemPaciente(Paciente paciente) {
+        this(paciente.getNome(), paciente.getEmail(), paciente.getCpf());
+    }
+}

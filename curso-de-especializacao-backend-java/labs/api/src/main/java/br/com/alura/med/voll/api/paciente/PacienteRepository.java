05678c593514e25f2d9fdf0ae0ed5218aa9ef5f5
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2023-09-27 01:49:33 -0300

    feat: deleted paciente

--- curso-de-especializacao-backend-java/labs/api/src/main/java/br/com/alura/med/voll/api/paciente/PacienteRepository.java
+++ curso-de-especializacao-backend-java/labs/api/src/main/java/br/com/alura/med/voll/api/paciente/PacienteRepository.java
@@ -1,8 +1,11 @@
 package br.com.alura.med.voll.api.paciente;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+    Page<Paciente> findAllByAtivoTrue(Pageable paginacao);
 }

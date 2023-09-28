3ce1dc1aca02a36b202332fb4370aab6eeeab1a4
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2023-09-27 21:57:57 -0300

    feat: implemented usuario entity and migration

---
+++ curso-de-especializacao-backend-java/labs/api/src/main/java/br/com/alura/med/voll/api/domain/usuario/Usuario.java
@@ -0,0 +1,23 @@
+package br.com.alura.med.voll.api.domain.usuario;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Usuario")
+@Table(name = "usuarios")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String login;
+    private String senha;
+}
